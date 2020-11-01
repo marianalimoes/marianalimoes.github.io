4a9bc90c38613fc7ee1c38dc62697a63b5ac6dee
marianalimoes <66639848+marianalimoes@users.noreply.github.com>
2020-11-01 00:08:11 +0000

    all

---
+++ _root/projeto-script.js
@@ -0,0 +1,45 @@
+// only smartphone
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+// media query event handler
+if (matchMedia) {
+const mq = window.matchMedia("(max-width: 1025px)");
+mq.addListener(WidthChange);
+WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+if (mq.matches) {
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+
+    //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+  }
+} else {
+}
+
+}
